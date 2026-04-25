51005cb35d5be939556c8f47c8a144ad709d36e7
Satyam Gurjar <satyagurjar405@gmail.com>
2026-04-25 17:20:57 +0530

    add create embedding models

---
+++ GenAI/chatmodel/huggingface.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+response = model.invoke(
+    "What is a database?",
+    max_tokens=50,
+    temperature=0.7
+)
+
+print(response.content)
